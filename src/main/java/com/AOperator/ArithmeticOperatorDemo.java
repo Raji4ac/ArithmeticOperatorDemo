9a2f0069c489192d5167b7298645040c263e4087
raji <raji4ac@gmail.com>
2018-08-24 21:01:16 +0100

    arthimetic operators example

---
+++ src/main/java/com/AOperator/ArithmeticOperatorDemo.java
@@ -0,0 +1,29 @@
+public class ArithmeticOperatorDemo {
+	// Demonstrate the basic arithmetic operators.
+		public static void main(String args[]) {
+		// arithmetic using integers
+		System.out.println("Integer Arithmetic");
+		int i = 1 + 1;
+		int n = i * 3;
+		int m = n / 4;
+		int p = m - i;
+		int q = -p;
+		System.out.println("i = " + i);
+		System.out.println("n = " + n);
+		System.out.println("m = " + m);
+		System.out.println("p = " + p);
+		System.out.println("q = " + q);
+		// arithmetic using doubles
+		System.out.println("\nFloating Point Arithmetic");
+		double a = 1 + 1;
+		double b = a * 3;
+		double c = b / 4;
+		double d = c - a;
+		double e = -d;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+		System.out.println("e = " + e);
+	}
+}
